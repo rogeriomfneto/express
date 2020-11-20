ec1e50af14a6a08b4b47c62e047e0b96e18dff10
Rogério Marcos Fernandes Neto <rogeriomfneto@hotmail.com>
2020-11-19 23:14:36 -0300

    middleware

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.use('/opa', (req, res) => {
-  res.send('Estou bem!');
+app.use((req, res, next) => {
+  console.log("Antes...");
+  next();
+});
+
+app.get('/opa', (req, res, next) => {
+  console.log("Durante...");
+
+  res.json({
+    data: [
+      { id: 7, name: 'Ana', position: 1 },
+      { id: 34, name: 'Bia', position: 2 },
+      { id: 73, name: 'carlos', position: 3 },
+    ],
+    count: 30,
+    skip: 0,
+    limit: 3,
+    status: 200,
+  });
+
+  next();
+
+  // res.send('Estou bem!');
+});
+
+app.use((req, res) => {
+  console.log("Depois...");
 });
 
 app.listen(3000, () => {
